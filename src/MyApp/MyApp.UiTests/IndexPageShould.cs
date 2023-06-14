207e45611accefd9ac5322ae72fe630fe1cfdffb
David Perish <david@stonyhillsoftware.com>
2023-06-14 16:58:03 -0400

    Annotate lines in test class for begingers

--- src/MyApp/MyApp.UiTests/IndexPageShould.cs
+++ src/MyApp/MyApp.UiTests/IndexPageShould.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System.Drawing;                                                          // There are some hidden usings imported via globals in Usings.cs       
 using FluentAssertions;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -8,21 +8,21 @@ namespace MyApp.UiTests;
 
 public class IndexPageShould
 {
-    private const string BaseUrl = "https://localhost:7173/";
+    private const string BaseUrl = "https://localhost:7173/";                  // Any module level variables should be marked const when possible
 
-    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly ITestOutputHelper _testOutputHelper;                      // This will be dependency injected by xUnit, [CTRL] + [.] can auto gen the ctor, arg, assignments
 
     public IndexPageShould(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
     }
 
-    [Fact]
-    [Trait("Category", "Index")]
+    [Fact]                                                                     // Fact attributes are paramless, [Theory] attr would allow for test method args
+    [Trait("Category", "Index")]                                               // Traits are a way to group tests, can be used to filter tests in test explorer
     public async Task ContainTheCorrectHeadingText()
     {
         // Arrange
-        using var driver = new ChromeDriver();
+        using var driver = new ChromeDriver();                                 // This is new-school C# syntax for using a disposable object, it will be disposed at the end of the method scope
         const string expectedHeadingText = "Welcome";
         driver.Manage().Window.Size = new Size(480, 960);
         
@@ -35,13 +35,13 @@ public class IndexPageShould
         screenShot.SaveAsFile($"../../../ScreenShots/{fileName}", ScreenshotImageFormat.Png);
         _testOutputHelper.WriteLine($"Screenshot saved to {nameof(IndexPageShould)}_{nameof(ContainTheCorrectHeadingText)}_{DateTime.Now.Date:yyyy-MM-dd}.png");
 
-        // Assert
+        // Assert                                                              // Find element is using an extension method we created in /Extensions/WebDriverExtensions.cs
         var actualHeadingText = driver.FindElement(By.TagName("h1"), TimeSpan.FromSeconds(1)).Text;
         
         _testOutputHelper.WriteLine($"Actual heading text: {actualHeadingText}");
 
-        actualHeadingText.Should().BeEquivalentTo(expectedHeadingText);
+        actualHeadingText.Should().BeEquivalentTo(expectedHeadingText);        // Should() is another extension method, this one from FluentAssertions nuget package
 
-        driver.Quit();
+        driver.Quit();                                                         
     }
 }
